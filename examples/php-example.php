f78283156196ab6a717dae3997ceafa828b13ed0
Rudi Strydom <iam@thatguy.co.za>
2015-09-25 16:27:55 +0200

    My version of the example

--- examples/php-example.php
+++ examples/php-example.php
@@ -1,22 +1,79 @@
 <?php
 
+define('STATSD_HOST', 'graphite');
+define('STATSD_PORT', 8125);
+
 /**
  * Sends statistics to the stats daemon over UDP
- *
+ * https://github.com/stevenscg/statsd
  **/
-
 class StatsD {
 
-    /**
-     * Log timing information
-     *
-     * @param string $stats The metric to in log timing info for.
-     * @param float $time The ellapsed time (ms) to log
-     * @param float|1 $sampleRate the rate (0-1) for sampling.
-     **/
-    public static function timing($stat, $time, $sampleRate=1) {
-        StatsD::send(array($stat => "$time|ms"), $sampleRate);
+    public static $instance;
+    public $namespace;
+
+    function __construct($namespace="unknown")
+    {
+        $this->namespace = $namespace;
     }
+
+
+    static function getStatsD($namespace)
+    {
+        if (is_null(self::$instance)){
+            self::$instance = new StatsD($namespace);
+        }
+
+        return self::$instance;
+    }
+
+
+    /**
+     * Sets one or more timing values
+     *
+     * @param string|array $stats The metric(s) to set.
+     * @param float $time The elapsed time (ms) to log
+     **/
+    public static function timing($stats, $time)
+    {
+        $statKey = self::$instance->namespace.".".$stats;
+        StatsD::updateStats($statKey, $time, 1, 'ms');
+    }
+
+
+    /**
+     * Sets one or more gauges to a value
+     *
+     * @param string|array $stats The metric(s) to set.
+     * @param float $value The value for the stats.
+     **/
+    public static function gauge($stats, $value)
+    {
+        $statKey = self::$instance->namespace.".".$stats;
+        StatsD::updateStats($statKey, $value, 1, 'g');
+    }
+
+
+    /**
+     * A "Set" is a count of unique events.
+     * This data type acts like a counter, but supports counting
+     * of unique occurences of values between flushes. The backend
+     * receives the number of unique events that happened since
+     * the last flush.
+     *
+     * The reference use case involved tracking the number of active
+     * and logged in users by sending the current userId of a user
+     * with each request with a key of "uniques" (or similar).
+     *
+     * @param string|array $stats The metric(s) to set.
+     * @param float $value The value for the stats.
+     **/
+    public static function set($stats, $value)
+    {
+        $statKey = self::$instance->namespace.".".$stats;
+        StatsD::updateStats($statKey, $value, 1, 's');
+    }
+
 
     /**
      * Increments one or more stats counters
@@ -25,9 +82,26 @@ class StatsD {
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      * @return boolean
      **/
-    public static function increment($stats, $sampleRate=1) {
-        StatsD::updateStats($stats, 1, $sampleRate);
+    public static function increment($stats, $sampleRate=1)
+    {
+        $statKey = self::$instance->namespace.".".$stats;
+        StatsD::updateStats($statKey, 1, $sampleRate, 'c');
     }
+
+    /**
+     * Set a counter +X numbers
+     *
+     * @param string|array $stats The metric(s) to increment.
+     * @param int          $amount Amount to increase the counter with (1+)
+     * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @return boolean
+     **/
+    public static function counter($stats, $amount=1, $sampleRate=1)
+    {
+        $statKey = self::$instance->namespace.".".$stats;
+        StatsD::updateStats($statKey, $amount, $sampleRate, 'c');
+    }
+
 
     /**
      * Decrements one or more stats counters.
@@ -36,38 +110,40 @@ class StatsD {
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      * @return boolean
      **/
-    public static function decrement($stats, $sampleRate=1) {
-        StatsD::updateStats($stats, -1, $sampleRate);
+    public static function decrement($stats, $sampleRate=1)
+    {
+        $statKey = self::$instance->namespace.".".$stats;
+        StatsD::updateStats($statKey, -1, $sampleRate, 'c');
     }
 
+
     /**
-     * Updates one or more stats counters by arbitrary amounts.
+     * Updates one or more stats.
      *
      * @param string|array $stats The metric(s) to update. Should be either a string or array of metrics.
      * @param int|1 $delta The amount to increment/decrement each metric by.
      * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @param string|c $metric The metric type ("c" for count, "ms" for timing, "g" for gauge, "s" for set)
      * @return boolean
      **/
-    public static function updateStats($stats, $delta=1, $sampleRate=1) {
+    public static function updateStats($stats, $delta=1, $sampleRate=1, $metric='c')
+    {
         if (!is_array($stats)) { $stats = array($stats); }
         $data = array();
         foreach($stats as $stat) {
-            $data[$stat] = "$delta|c";
+            $data[$stat] = "$delta|$metric";
         }
-
         StatsD::send($data, $sampleRate);
     }
+
 
     /*
      * Squirt the metrics over UDP
      **/
-    public static function send($data, $sampleRate=1) {
-        $config = Config::getInstance();
-        if (! $config->isEnabled("statsd")) { return; }
-
+    public static function send($data, $sampleRate=1)
+    {
         // sampling
         $sampledData = array();
-
         if ($sampleRate < 1) {
             foreach ($data as $stat => $value) {
                 if ((mt_rand() / mt_getrandmax()) <= $sampleRate) {
@@ -77,13 +153,12 @@ class StatsD {
         } else {
             $sampledData = $data;
         }
-
+        
         if (empty($sampledData)) { return; }
-
         // Wrap this in a try/catch - failures in any of this should be silently ignored
         try {
-            $host = $config->getConfig("statsd.host");
-            $port = $config->getConfig("statsd.port");
+            $host = STATSD_HOST;
+            $port = STATSD_PORT;
             $fp = fsockopen("udp://$host", $port, $errno, $errstr);
             if (! $fp) { return; }
             foreach ($sampledData as $stat => $value) {
@@ -94,47 +169,3 @@ class StatsD {
         }
     }
 }
-
-class Config
-{
-    private static $_instance;
-    private $_data;
-
-    private function __construct()
-    {
-        $this->_data = parse_ini_file('statsd.ini', true);
-    }
-
-    public static function getInstance()
-    {
-        if (!self::$_instance) self::$_instance = new self();
-
-        return self::$_instance;
-    }
-
-    public function isEnabled($section)
-    {
-        return isset($this->_data[$section]);
-    }
-
-    public function getConfig($name)
-    {
-        $name_array = explode('.', $name, 2);
-
-        if (count($name_array) < 2) return;
-
-        list($section, $param) = $name_array;
-
-        if (!isset($this->_data[$section][$param])) return;
-
-        return $this->_data[$section][$param];
-    }
-}
-
-/* Config file example (put it into "statsd.ini"):
-
-[statsd]
-host = yourhost
-port = 8125
-
-*/
